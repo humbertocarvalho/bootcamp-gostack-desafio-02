3f2b5ce91f2f20a09090a2629f2194e52bd61d48
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-23 19:26:26 -0300

    Add initial files.

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'basad' });
-});
+// Rotas do Usuário
+routes.get('/user/:id', (req, res) => {});
+routes.post('/user', (req, res) => {});
+routes.put('/user', (req, res) => {});
+routes.delete('/user', (req, res) => {});
+
+// Rotas de Autenticação
+routes.post('/session', (req, res) => {});
 
 export default routes;

02e2165e071d326451f1e11099db24b9aab0d780
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-01 12:14:35 -0300

    Create file controller.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 // Rotas do Usuário);
 routes.post('/user', UserController.store);
@@ -12,5 +16,13 @@ routes.put('/user', authMiddleware, UserController.update);
 
 // Rotas de Autenticação
 routes.post('/session', SessionController.store);
+
+// Upload de arquivo
+routes.post(
+  '/files',
+  upload.single('file'),
+  authMiddleware,
+  FileController.store
+);
 
 export default routes;

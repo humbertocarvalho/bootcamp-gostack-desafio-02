ed7b15e11250babf7f275d6d8beba72a3fb882e6
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-18 07:02:22 -0300

    Add banner relation.

--- src/app/models/Meetup.js
+++ src/app/models/Meetup.js
@@ -26,6 +26,7 @@ class Meetup extends Model {
 
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'host_id', as: 'host' });
+    this.belongsTo(models.File, { foreignKey: 'banner_id', as: 'banner' });
   }
 }
 
